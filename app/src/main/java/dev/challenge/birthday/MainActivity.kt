fa7d6f7ec2e665e24acc0bd82c3df6b369672292
Walter Banda <walterkaimurima@gmail.com>
2022-11-25 18:50:40 +0300

    ☕ Added splash screen

--- app/src/main/java/dev/challenge/birthday/MainActivity.kt
+++ app/src/main/java/dev/challenge/birthday/MainActivity.kt
@@ -2,9 +2,11 @@ package dev.challenge.birthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        var splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
